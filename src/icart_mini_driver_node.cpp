d5195c41b97828b3c9ed8be41fbae923283c5d2c
haruyama8940 <yaruo0406@gmail.com>
2023-02-28 05:47:51 +0900

    devel sub cmd_vel

--- src/icart_mini_driver_node.cpp
+++ src/icart_mini_driver_node.cpp
@@ -1,10 +1,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-
+#include "nav_msgs/msg/odometry.hpp"
+// #include "tf"
 
 namespace YP
 {
-#include <ypspur,h>
+#include <ypspur.h>
 }
 
 class Icart_mini_driver : public rclcpp::Node
@@ -13,14 +14,35 @@ class Icart_mini_driver : public rclcpp::Node
         Icart_mini_driver()
         : Node("icart_moni_driver")
         {
+        // setParam();
+        // getParam();
         cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
-        "cmd_vel", 10, std::bind(&Icart_mini_driver::cmd_vel_cb, this, _1));
-        geometry_msgs::msg::Twist::ConstPtr cmd_vel_;
+        "cmd_vel", 10, std::bind(&Icart_mini_driver::cmd_vel_cb, this, std::placeholders::_1));
+        // geometry_msgs::msg::Twist::ConstPtr cmd_vel_;
         }
     private:
-        void cmd_vel_cb(const geometry_msgs::msg::Twist::SharedPtr msg) const
+        geometry_msgs::msg::Twist::SharedPtr cmd_vel_;
+        // void cmd_vel_cb(const geometry_msgs::msg::Twist::SharedPtr msg) const;
+        void cmd_vel_cb(const geometry_msgs::msg::Twist::SharedPtr msg)
         {
-            cmd_vel_ msg
+            cmd_vel_ =  msg;
+            // https://github.com/openspur/yp-spur/blob/master/doc/Manpage.control.md#velocity-control
+            YP::YPSpur_vel(msg->angular.x,msg->angular.z);
         }
-        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_
+
+        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
+};
+    // void cmd_vel_cb(const geometry_msgs::msg::Twist::SharedPtr msg)
+    //  {
+    //         // cmd_vel_ =  msg;
+    //         // https://github.com/openspur/yp-spur/blob/master/doc/Manpage.control.md#velocity-control
+    //         YP::YPSpur_vel(msg->angular.x,msg->angular.z);
+    //  }
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Icart_mini_driver>());
+  rclcpp::shutdown();
+  return 0;
 }
