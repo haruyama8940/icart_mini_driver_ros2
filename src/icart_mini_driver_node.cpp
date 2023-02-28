c7de03a4c42d7dfa91b63bd300d39109b7af4191
haruyma8940 <yaruo0406@gmail.com>
2023-02-28 20:51:06 +0900

    add class

--- src/icart_mini_driver_node.cpp
+++ src/icart_mini_driver_node.cpp
@@ -12,33 +12,55 @@ class Icart_mini_driver : public rclcpp::Node
 {
     public:
         Icart_mini_driver()
-        : Node("icart_moni_driver")
-        {
+        : Node("icart_mini_driver")
+        {}
         // setParam();
         // getParam();
-        cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
-        "cmd_vel", 10, std::bind(&Icart_mini_driver::cmd_vel_cb, this, std::placeholders::_1));
+        // cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
+        // "cmd_vel", 10, std::bind(&Icart_mini_driver::cmd_vel_cb, this, std::placeholders::_1));
         // geometry_msgs::msg::Twist::ConstPtr cmd_vel_;
-        }
+        void cmd_vel_cb(const geometry_msgs::msg::Twist::SharedPtr msg);
+        void read_param();
+        void bringup_ypspur(std::vector<std::string> args);
+    
     private:
         geometry_msgs::msg::Twist::SharedPtr cmd_vel_;
+        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub;
         // void cmd_vel_cb(const geometry_msgs::msg::Twist::SharedPtr msg) const;
-        void cmd_vel_cb(const geometry_msgs::msg::Twist::SharedPtr msg)
+        
+};
+    void Icart_mini_driver::read_param()
+    {  
+
+    }
+    void Icart_mini_driver::cmd_vel_cb(const geometry_msgs::msg::Twist::SharedPtr msg)
         {
             cmd_vel_ =  msg;
             // https://github.com/openspur/yp-spur/blob/master/doc/Manpage.control.md#velocity-control
-            YP::YPSpur_vel(msg->angular.x,msg->angular.z);
+            YP::YPSpur_vel(msg->linear.x,msg->angular.z);
         }
-
-        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
-};
+        
     // void cmd_vel_cb(const geometry_msgs::msg::Twist::SharedPtr msg)
     //  {
     //         // cmd_vel_ =  msg;
     //         // https://github.com/openspur/yp-spur/blob/master/doc/Manpage.control.md#velocity-control
     //         YP::YPSpur_vel(msg->angular.x,msg->angular.z);
     //  }
-
+    //  if (YP::YP_get_error_state() == 0)
+    //     {
+            
+    //     }
+    // void Icart_mini_driver::bringup_ypspur(std::vector<std::string> args)
+    // {
+    //     std::vector<std::string> args =
+    //         {
+    //           ypspur_bin_,
+    //           "-d", port_,
+    //           "--admask", ad_mask,
+    //           "--msq-key", std::to_string(key_)
+    //         };
+    //     system("ypspur-coordinator -p  -d /dev/sensors/icart-mini");
+    // }
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
